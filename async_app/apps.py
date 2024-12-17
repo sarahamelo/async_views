0ab62303af08d172d3cf2d86892d348598c3dc80
sarahamelo <sarah.alves.melo@hotmail.com>
2024-12-17 15:38:55 -0300

    pushing

--- async_app/apps.py
+++ async_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AsyncAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'async_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "async_app"
